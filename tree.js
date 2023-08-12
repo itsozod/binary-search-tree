d925434563e9e7d3b6d4a96ecd14cc440297fef9
Ozod <bukhorizodao@gmail.com>
2023-08-13 02:59:06 +0500

    isEmpty method and sorted method

--- tree.js
+++ tree.js
@@ -10,4 +10,15 @@ class binarySearchTree {
   constructor(array) {
     this.root = this.buildTree(array);
   }
+
+  isEmpty() {
+    return this.root === null;
+  }
+  sortAndRemoveDubs(array) {
+    const sorted = array.sort((a, b) => a - b);
+    const removedDublicates = sorted.filter(
+      (value, index) => sorted.indexOf(value) === index
+    );
+    return removedDublicates;
+  }
 }
